105c051915f5167aca577fe52ea35822f84733ef
Vitaliy123 <cpc1251@mail.ru>
2022-04-10 21:40:16 +0300

    5.1.2. Создать итератор четные числа [#150 #286132] (repaired the build)

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -14,7 +14,10 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-
+        while (row < data.length && data[row].length == column) {
+            row++;
+            column = 0;
+        }
         return row < data.length;
     }
 
