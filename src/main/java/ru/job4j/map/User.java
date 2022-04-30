c3d16321da9e452eff958b86e107934c51e53238
Vitaliy123 <cpc1251@mail.ru>
2022-04-30 18:04:05 +0300

    1. Создать модель User [#999]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
+import java.util.spi.CalendarDataProvider;
 
 public class User {
     private String name;
@@ -14,6 +15,35 @@ public class User {
     }
 
     public User() {
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && name.equals(user.name) && birthday.equals(user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        Calendar date = new GregorianCalendar(2017, Calendar.JANUARY, 25);
+        User user1 = new User("Ivan", 1, date);
+        User user2 = new User("Ivan", 1, date);
+        Map<User, Object> users = new HashMap<>();
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+        System.out.println(user1.equals(user2));
+        System.out.println(users);
     }
 }
