cff09e34ab4dea04cadf3fcf30badb385558e61a
Vitaliy123 <cpc1251@mail.ru>
2022-05-23 05:58:32 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,31 +1,26 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> collect;
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             boolean b = false;
-            collect = in.lines().collect(Collectors.toList());
-            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                String tmp = "";
-                for (String s : collect) {
-                    String tmp1 = s.substring(s.indexOf(' ') + 1) + ";";
-                    if ((s.startsWith("400") || s.startsWith("500")) && !b) {
-                        tmp = "";
-                        b = true;
-                        tmp += tmp1;
-                    } else if ((s.startsWith("200") || s.startsWith("30000")) && b) {
-                        b = false;
-                        tmp += tmp1;
-                        out.println(tmp);
-                    }
+            String tmp = "";
+            String s = in.readLine();
+            while (s != null) {
+                String tmp1 = s.substring(s.indexOf(' ') + 1) + ";";
+                if ((s.startsWith("400") || s.startsWith("500")) && !b) {
+                    tmp = "";
+                    b = true;
+                    tmp += tmp1;
+                } else if ((s.startsWith("200") || s.startsWith("300")) && b) {
+                    b = false;
+                    tmp += tmp1;
+                    out.println(tmp);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                s = in.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
