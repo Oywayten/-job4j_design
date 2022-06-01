87c156f90cf88f45695474ec26a2011f2a0589ac
Vitaliy123 <cpc1251@mail.ru>
2022-06-01 23:02:11 +0300

    3. Бот [#7921 #307997]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -18,12 +18,10 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
                     if (str.contains("/?msg=Hello")) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         out.write("Hello, dear friend.".getBytes());
                     } else if (str.contains("/?msg=Exit")) {
                         server.close();
                     } else {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         out.write("What".getBytes());
                     }
                     out.flush();
