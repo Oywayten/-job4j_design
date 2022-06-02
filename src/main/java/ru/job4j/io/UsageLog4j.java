a343829679e9e3a14cd503abd35cadcbedb16c4b
Vitaliy123 <cpc1251@mail.ru>
2022-06-02 08:59:40 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,8 +8,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+        boolean isTeacher = true;
+        String courseName = "job4j";
+        String startDate = "18.01.2022 19:41";
+        String lastActivity = "02.06.2022 08:35";
+        int daysOfLearning = 135;
+        float tasksPerDay = 2.41f;
         LOG.trace("trace message");
-        LOG.debug("debug message");
+        LOG.debug("\nUser info name : {}, age : {} , he is a teacher on the {} course and it is {}.\n"
+                        .concat("My learning progress : Начало обучения {}, "
+                                .concat("последняя активность : {} , дней обучения {}, задач в день {}.")),
+                name, age, courseName, isTeacher, startDate, lastActivity, daysOfLearning, tasksPerDay);
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
