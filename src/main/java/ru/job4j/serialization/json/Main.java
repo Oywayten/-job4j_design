0878c388d498da0273c367d839b8a96cdc880d52
Vitaliy123 <cpc1251@mail.ru>
2022-06-02 22:17:14 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -0,0 +1,22 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        final Truck truck = new Truck(true, 20, "Volvo 200",
+                new Contact("+79110010011"), new String[]{"Viktor", "Maria"});
+        final Gson gson = new GsonBuilder().create();
+        System.out.println();
+        System.out.println(gson.toJson(truck));
+
+        final String trackJson = "{\"isTipper\":false,"
+                + "\"loadCapacity\":3,"
+                + "\"model\":\"Gazel\","
+                + "\"contact\":{\"phone\":\"+79134305983\"},"
+                + "\"passengers\":[\"Arnold\",\"forbidden\"]}";
+        final Truck trackMod = gson.fromJson(trackJson, Truck.class);
+        System.out.println(trackMod);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/Truck.java
@@ -0,0 +1,25 @@
+package ru.job4j.serialization.json;
+
+import java.util.Arrays;
+
+public class Truck {
+    private final boolean isTipper;
+    private final int loadCapacity;
+    private final String model;
+    private final Contact contact;
+    private final String[] passengers;
+
+    public Truck(boolean isTipper, int loadCapacity, String model, Contact contact, String[] passengers) {
+        this.isTipper = isTipper;
+        this.loadCapacity = loadCapacity;
+        this.model = model;
+        this.contact = contact;
+        this.passengers = passengers;
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" + "isTipper=" + isTipper + ", loadCapacity=" + loadCapacity + ", model='" + model
+                + '\'' + ", contact=" + contact + ", passengers=" + Arrays.toString(passengers) + '}';
+    }
+}
