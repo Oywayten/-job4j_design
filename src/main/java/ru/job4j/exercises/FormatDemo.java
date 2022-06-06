e78d4b14c0699d827d852526e4d99b4174b3335d
Vitaliy123 <cpc1251@mail.ru>
2022-06-06 11:01:08 +0300

    Formatter tested

--- src/main/java/ru/job4j/exercises/FormatDemo.java
+++ src/main/java/ru/job4j/exercises/FormatDemo.java
@@ -10,13 +10,16 @@ public class FormatDemo {
         formatter.format("It's very easy to format with %s %d %f", "Java", 10, 98.86);
         System.out.println(formatter);
         Calendar calendar = Calendar.getInstance();
+        formatter.close();
         formatter = new Formatter();
         formatter.format("%tc", calendar);
         System.out.println(formatter);
+        formatter.close();
         formatter = new Formatter();
         formatter.format("%tb", calendar);
         System.out.println(formatter);
         calendar.set(Calendar.MONTH, 10);
+        formatter.close();
         formatter = new Formatter();
         formatter.format("%tb", calendar);
         System.out.println(formatter);
