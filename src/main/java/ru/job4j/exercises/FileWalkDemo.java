bf61e39bff48f9c45fe5a0c64b8c50a5e95f2f90
Vitaliy123 <cpc1251@mail.ru>
2022-06-09 13:51:11 +0300

    Files walk tested

---
+++ src/main/java/ru/job4j/exercises/FileWalkDemo.java
@@ -0,0 +1,31 @@
+package ru.job4j.exercises;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileWalkDemo {
+    public static void main(String[] args) {
+        Path source = Paths.get("src/main/java/ru/job4j/exercises/serialization");
+        Path target = Paths.get("src/main/java/ru/job4j/exercises/temp");
+        try {
+            Files.walk(source).forEach(p -> {
+                try {
+                    Path q = target.resolve(source.relativize(p));
+                    if (Files.isDirectory(p) && !Files.exists(p)) {
+                        Files.createDirectory(q);
+                    } else {
+                        if (!Files.exists(q)) {
+                            Files.copy(p, q);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
