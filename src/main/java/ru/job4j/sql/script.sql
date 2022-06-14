080866d60746f2f479867cd06c7ff9c97ab0426b
Vitaliy123 <cpc1251@mail.ru>
2022-06-14 19:56:14 +0300

    minor changes

--- src/main/java/ru/job4j/sql/script.sql
+++ src/main/java/ru/job4j/sql/script.sql
@@ -6,11 +6,11 @@ manager varchar(255),
 start date,
 peoples integer);
 
-insert into projects(manager, start, peoples) values('Vitaliy', '12.06.2022', '7');
+insert into projects(manager, dstart, peoples) values('Vitaliy', '12.06.2022', '7');
 
 select * from projects;
 
-update projects set start = '12.03.2022';
+update projects set dstart = '12.03.2022';
 
 select * from projects;
 
