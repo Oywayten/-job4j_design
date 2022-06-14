566c648a83f3775cdd524494d71af605de547dbc
Vitaliy123 <cpc1251@mail.ru>
2022-06-14 21:43:44 +0300

    1. Схема прав пользователи и роли. [#1093]

--- src/main/java/ru/job4j/sql/items/create.sql
+++ src/main/java/ru/job4j/sql/items/create.sql
@@ -1,33 +1,34 @@
 create table role(
     id serial primary key ,
     fname varchar(255)
-)
+);
 
 create table users(
     id serial primary key ,
     fname varchar(255),
     id_role int references role(id)
-)
+);
 
 create table rules(
     id serial primary key ,
     fname varchar(255)
-)
+);
 
 create table role_rules(
     id serial primary key ,
     id_role int references role(id),
     id_rules int references rules(id)
-)
+);
+
 create table category(
     id serial primary key ,
     fname varchar(255)
-)
+);
 
 create table state(
     id serial primary key ,
     fname varchar(255)
-)
+);
 
 create table item(
     id serial primary key ,
@@ -36,16 +37,16 @@ create table item(
     id_users int references users(id),
     id_category int references category(id),
     id_state int references state(id)
-)
+);
 
 create table comments(
     id serial primary key ,
     ftext text,
     id_item int references item(id)
-)
+);
 
 create table attachs(
     id serial primary key ,
     file varchar(255),
     id_item int references item(id)
-)
+);
