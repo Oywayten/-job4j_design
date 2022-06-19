fda3c6fd5ca09925ccf714ee5e677538d0e2c487
Vitaliy123 <cpc1251@mail.ru>
2022-06-19 23:29:29 +0300

    minor changes

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -3,22 +3,51 @@ package ru.job4j.jdbc;
 import ru.job4j.io.Config;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.StringJoiner;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "create table if not exists demo_table(%s, %s);",
+                        "id serial primary key",
+                        "name text"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "demo_table"));
+            }
+        }
+    }
+
+    private static Connection getConnection() throws Exception {
         Class.forName("org.postgresql.Driver");
         Config config = new Config("src/main/java/ru/job4j/jdbc/app.properties");
         config.load();
         String url = config.value("url");
         String login = config.value("login");
         String password = config.value("password");
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        String rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        String header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        StringJoiner buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selectionResultSet = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selectionResultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
         }
+        return buffer.toString();
     }
 }
