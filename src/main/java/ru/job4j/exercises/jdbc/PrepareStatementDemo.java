076aed79c5a79922aed343d6ca0dd8a3ed1bb085
Vitaliy123 <cpc1251@mail.ru>
2022-06-21 23:14:47 +0300

    0.2. PrepareStatement [#379307] -demo for the lesson

---
+++ src/main/java/ru/job4j/exercises/jdbc/PrepareStatementDemo.java
@@ -0,0 +1,69 @@
+package ru.job4j.exercises.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PrepareStatementDemo {
+    private Connection connection;
+
+    public PrepareStatementDemo(Connection connection) throws Exception {
+        initConnection();
+    }
+
+    private void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public City insert(City city) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    city.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return city;
+    }
+
+    public boolean update(City city) {
+        boolean result = false;
+        try (PreparedStatement statement = connection.prepareStatement("update cities set name = ?, population = ? where id = ?")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.setInt(3, city.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public java.util.List<City> findAll() {
+        java.util.List<City> cities = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("select * from cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    cities.add(new City(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("population")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+}
---
+++ src/main/java/ru/job4j/exercises/jdbc/cities.sql
@@ -0,0 +1,5 @@
+create table cities(
+    id serial primary key ,
+    fname text,
+    population int
+);
