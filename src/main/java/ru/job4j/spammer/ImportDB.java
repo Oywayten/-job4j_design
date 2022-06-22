680d2fa79bda041dfe768e99e643bf5e9249cd35
Vitaliy123 <cpc1251@mail.ru>
2022-06-22 08:35:14 +0300

    0.2. PrepareStatement [#379307 #315137] - скорректировал метод load() и задокументировал поля и методы

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -4,19 +4,28 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * @author Oywayten
+ * version 1.01 2022-06-22
+ * Класс для импорта списка спамероd (типа User), из файла dump, в базу spammer
+ */
 public class ImportDB {
 
+    /**
+     * В поле хранятся параметры для подключения к базе из файла app1.properties
+     */
     private final Properties cfg;
+
+    /**
+     * Строковое поле с содержимым файла списока спамеров для добавления в базу
+     */
     private final String dump;
 
     public ImportDB(Properties cfg, String dump) {
@@ -30,19 +39,30 @@ public class ImportDB {
             cfg.load(in);
         }
         ImportDB db = new ImportDB(cfg, "./dump.txt");
-        Path path = Paths.get("dump.txt");
         db.save(db.load());
     }
 
+    /**
+     * Метод создает экземпляры пользователей на основании поля dump
+     *
+     * @return список пользователей типа List<User>
+     * @throws IOException ошибка потока ввода-вывода
+     */
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            rd.lines().forEach(s -> users.add(new User(s.split(";")[0], s.split(";")[1])));
+            rd.lines().map(s -> s.split(";")).forEach(s -> users.add(new User(s[0], s[1])));
         }
         return users;
     }
 
-    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+    /**
+     * Метод добавляет пользователей в базу
+     *
+     * @param users список пользователей типа List<Uses> для добавления в базу
+     * @throws Exception ошибка
+     */
+    public void save(List<User> users) throws Exception {
         Class.forName(cfg.getProperty("jdbc.driver"));
         try (Connection cnt = DriverManager.getConnection(
                 cfg.getProperty("jdbc.url"),
@@ -59,6 +79,9 @@ public class ImportDB {
         }
     }
 
+    /**
+     * Пользователь для добавления в базу
+     */
     private static class User {
         String name;
         String email;
