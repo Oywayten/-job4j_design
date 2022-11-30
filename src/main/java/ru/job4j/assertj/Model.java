b30211e32752f86b1b77a890656cb4a42c286bea
Vitaliy123 <cpc1251@mail.ru>
2022-11-30 21:49:14 +0300

    Подключение библиотеки AssertJ [#504881]

---
+++ src/main/java/ru/job4j/assertj/Model.java
@@ -0,0 +1,34 @@
+package ru.job4j.assertj;
+
+/**
+ * Created by Oywayten on 30.11.2022.
+ */
+public class Model {
+    private final int top;
+    private final double num;
+    private final String line;
+    private final boolean condition;
+
+    public Model(int top, double num, String line, boolean condition) {
+        this.top = top;
+        this.num = num;
+        this.line = line;
+        this.condition = condition;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public boolean isCondition() {
+        return condition;
+    }
+}
