7e27813578584fad5890c487d3ffd452a36bbc16
Vitaliy123 <cpc1251@mail.ru>
2022-11-30 21:33:02 +0300

    Подключение библиотеки AssertJ [#504881]

---
+++ src/main/java/ru/job4j/iterator/ArrayIt.java
@@ -0,0 +1,25 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+/**
+ * Created by Oywayten on 30.11.2022.
+ */
+public class ArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public ArrayIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        return data[point++];
+    }
+}
