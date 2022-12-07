a69185241364e1482f5b9e3a784e4d7444fc9435
Vitaliy123 <cpc1251@mail.ru>
2022-12-07 22:13:50 +0300

    Создал большой запрос, который разместил в представлении. На его основе сделал select.

--- src/main/java/ru/job4j/sql/orders/create.sql
+++ src/main/java/ru/job4j/sql/orders/create.sql
@@ -1,62 +1,74 @@
-CREATE TABLE students (
-    id serial PRIMARY KEY,
+create table if not exists students (
+    id serial primary key,
     fname varchar(50)
 )
 
-INSERT INTO students (fname)
-    VALUES
+insert into students (fname)
+    values
         ('Иван Иванов'),
         ('Петр Петрович');
 
-CREATE TABLE authors (
-    id serial PRIMARY KEY,
+create table if not exists authors (
+    id serial primary key,
     fname varchar(50)
 )
 
-INSERT INTO authors (fname)
-    VALUES
+insert into authors (fname)
+    values
         ('Александр Пушкин'),
         ('Николай Гоголь');
 
-CREATE TABLE books (
-    id serial PRIMARY KEY,
+create table if not exists books (
+    id serial primary key,
     fname varchar(50),
-    author_id integer REFERENCES authors(id)
+    author_id integer references authors(id)
 );
 
-INSERT INTO books (fname, author_id) VALUES ('Евгений Онегин', 1);
-INSERT INTO books (fname, author_id) VALUES ('Капитанская дочка', 1);
-INSERT INTO books (fname, author_id) VALUES ('Дубровский', 1);
-INSERT INTO books (fname, author_id) VALUES ('Мертвые души', 2);
-INSERT INTO books (fname, author_id) VALUES ('Вий', 2);
+insert into books (fname, author_id) values ('Евгений Онегин', 1);
+insert into books (fname, author_id) values ('Капитанская дочка', 1);
+insert into books (fname, author_id) values ('Дубровский', 1);
+insert into books (fname, author_id) values ('Мертвые души', 2);
+insert into books (fname, author_id) values ('Вий', 2);
 
-CREATE TABLE orders (
-    id serial PRIMARY KEY,
-    active boolean DEFAULT TRUE,
-    book_id integer REFERENCES books("id"),
-    student_id integer REFERENCES students("id")
+create table if not exists orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books("id"),
+    student_id integer references students("id")
 );
 
-INSERT INTO orders (book_id, student_id)
-    VALUES
+insert into orders (book_id, student_id)
+    values
         (1, 1),
         (3, 1),
         (5, 2),
         (4, 1),
         (2, 2);
 
-CREATE VIEW show_students_with_2_or_more_books AS
-    SELECT s.fname AS student, count(a.fname), a.fname AS author FROM students s
-        JOIN orders o ON s.id = o.student_id
-        JOIN books b ON o.book_id = b."id"
-        JOIN authors a ON b.author_id = a."id"
-        GROUP  BY (s.fname, a.fname) HAVING count(a.fname) >= 2;
+create view show_students_with_2_or_more_books as
+    select s.fname as student, count(a.fname), a.fname as author from students s
+        join orders o on s.id = o.student_id
+        join books b on o.book_id = b."id"
+        join authors a on b.author_id = a."id"
+        group  by (s.fname, a.fname) having count(a.fname) >= 2;
 
-
-SELECT * FROM show_students_with_2_or_more_books;
-
-ALTER view show_students_with_2_or_more_books
+alter view show_students_with_2_or_more_books
 rename to show_students_with_equals_or_more_then_2_books;
 
-DROP VIEW show_students_with_equals_or_more_then_2_books;
+select * from show_students_with_equals_or_more_then_2_books;
 
+drop view show_students_with_equals_or_more_then_2_books;
+
+--студенты у которых среднее количество книг по авторам больше 2
+create view avg_authors_books_per_student as
+select student, avg(count_books) as av from(
+select s.fname as student, a.fname as author, count(b.fname) as count_books from students as s
+join orders as o
+    on s.id = o.student_id
+join books as b
+    on o.book_id = b.id
+join authors as a
+    on b.author_id = a."id"
+group by student, author) as foo group by student;
+
+select * from avg_authors_books_per_student where av > 2;
